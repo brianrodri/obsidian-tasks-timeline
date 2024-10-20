752a89a255203cdeefea76b5c7e6488f38c3082b
Brian Rodriguez <brian@brianrodri.com>
2024-10-20 11:32:31 -0400

    feat: remove metadata from task text

--- src/components/tasks/task-entry.tsx
+++ src/components/tasks/task-entry.tsx
@@ -1,3 +1,4 @@
+import { memoize } from "lodash";
 import { Task } from "../../compat/dataview-types";
 import { TaskCheckbox } from "./task-checkbox";
 import { DateInfo } from "./task-date-info";
@@ -15,7 +16,7 @@ export const TaskEntry = ({ task }: TaskEntryProps) => (
             <div class="stripe" />
         </div>
         <div class="lines">
-            <div class="content">{task.text}</div>
+            <div class="content">{removePatterns(task.text)}</div>
             <div class="line info">
                 <LinkInfo link={task.section} />
                 <TagsInfo tags={task.tags} />
@@ -24,3 +25,19 @@ export const TaskEntry = ({ task }: TaskEntryProps) => (
         </div>
     </div>
 );
+
+const removePatterns = memoize((inputText: string) =>
+    [
+        TASK_RECURRENCE_EMOJI_PATTERN,
+        TASK_DATE_EMOJI_PATTERN,
+        TASK_PRIORITY_EMOJI_PATTERN,
+        DATAVIEW_FULL_PROPERTY_PATTERN,
+        DATAVIEW_SHORT_PROPERTY_PATTERN,
+    ].reduce((text: string, pattern: RegExp) => text.replace(pattern, ""), inputText),
+);
+
+const TASK_RECURRENCE_EMOJI_PATTERN = /🔁\s+([A-Za-z0-9\s,]+)/g;
+const TASK_PRIORITY_EMOJI_PATTERN = /(⏬|🔽|🔼|⏫|🔺)/g;
+const TASK_DATE_EMOJI_PATTERN = /(➕|⏳|🛫|📅|✅|❌) \d{4}-\d{2}-\d{2}/g;
+const DATAVIEW_FULL_PROPERTY_PATTERN = /\(\s*(.*?)\s*::\s*(.*?)\)/g;
+const DATAVIEW_SHORT_PROPERTY_PATTERN = /\[\s*(.*?)\s*::\s*(.*?)\]/g;
