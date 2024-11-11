2c1102a8488e929b1e14b4a33882b0ef08e083f6
Brian Rodriguez <brian@brianrodri.com>
2024-11-11 16:53:09 -0500

    test!: delete deprecated tests

--- src/components/__tests__/icons.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from "@testing-library/preact";
-import { expect, test } from "vitest";
-
-import * as Icons from "../icons";
-
-test.each(Object.entries(Icons))("%s is rendered", (_, Icon) => {
-    expect(() => render(<Icon />)).not.toThrow();
-});
