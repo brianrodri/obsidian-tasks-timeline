2847a6cfa38401bf276176fcec89c6f1ddb86361
Brian Rodriguez <brian@brianrodri.com>
2025-01-21 15:00:40 -0500

    chore: test upgrade to ESLint v9

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,16 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import js from "@eslint/js";
 import typescriptPlugin from "@typescript-eslint/eslint-plugin";
 import typescriptPluginParser from "@typescript-eslint/parser";
+import preactConfig from "eslint-config-preact";
 import prettierConfig from "eslint-config-prettier";
 import prettierPlugin from "eslint-plugin-prettier";
 import reactHooksPlugin from "eslint-plugin-react-hooks";
 import globals from "globals";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    resolvePluginsRelativeTo: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all,
-});
 
 /** @type { import("eslint").Linter.Config[] } */
 export default [
     { ignores: ["coverage/", "docs/", "dist/", "node_modules/", "test-vault/"] },
 
-    ...compat.extends("preact"),
+    ...preactConfig.flat,
 
     {
         files: ["*.config.js", ".husky/install.js"],
