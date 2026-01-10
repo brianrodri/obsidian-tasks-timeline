a5249fef287b7b93d97d6f4d4eea40d4a729ecae
Brian Rodriguez <brian@brianrodri.com>
2026-01-10 11:56:51 -0500

    fix(styles): display task description and metadata pills inline

--- src/layout/task-entry.tsx
+++ src/layout/task-entry.tsx
@@ -51,42 +51,40 @@ export const TaskEntry = ({ task }: TaskEntryProps) => {
                 <TaskCheckbox status={task.status} location={task.location} />
             </div>
             <div class="lines">
-                <div class="content">
+                <span class="content">
                     <ObsidianMarkdown
                         app={plugin.app}
                         component={plugin}
                         markdown={task.description}
                         sourcePath={filePath}
                     />
-                </div>
-                <div class="line info">
-                    <TaskInfoEntry symbol={<FileIcon />} className="file">
-                        <VaultLink className="internal-link" href={obsidianHref} sourcePath={filePath}>
-                            {fileLabel}
-                        </VaultLink>
-                    </TaskInfoEntry>
-                    <TaskInfoEntry symbol={<SignatureIcon />} className="id">
-                        {task.id}
-                    </TaskInfoEntry>
-                    <TaskInfoEntry symbol={<TagsIcon />} className="tag">
-                        {task.tags.values().toArray().join(", ")}
-                    </TaskInfoEntry>
-                    <TaskInfoEntry symbol={<PriorityIcon />} className="priority">
-                        {task.priority}
-                    </TaskInfoEntry>
-                    <TaskInfoEntry symbol={<RepeatIcon />} className="repeat">
-                        {task.recurrenceRule}
-                    </TaskInfoEntry>
-                    <TaskInfoEntry symbol={<NetworkIcon />} className="depends-on">
-                        {task.dependsOn.values().toArray().join(", ")}
-                    </TaskInfoEntry>
-                    <TaskDateInput field="dueDate" symbol={<DueIcon />} task={task} />
-                    <TaskDateInput field="doneDate" symbol={<CompletedIcon />} task={task} />
-                    <TaskDateInput field="cancelledDate" symbol={<CancelledIcon />} task={task} />
-                    <TaskDateInput field="scheduledDate" symbol={<ScheduledIcon />} task={task} />
-                    <TaskDateInput field="startDate" symbol={<StartedIcon />} task={task} />
-                    <TaskDateInput field="createdDate" symbol={<CreatedIcon />} task={task} />
-                </div>
+                </span>
+                <TaskInfoEntry symbol={<FileIcon />} className="file">
+                    <VaultLink className="internal-link" href={obsidianHref} sourcePath={filePath}>
+                        {fileLabel}
+                    </VaultLink>
+                </TaskInfoEntry>
+                <TaskInfoEntry symbol={<SignatureIcon />} className="id">
+                    {task.id}
+                </TaskInfoEntry>
+                <TaskInfoEntry symbol={<TagsIcon />} className="tag">
+                    {task.tags.values().toArray().join(", ")}
+                </TaskInfoEntry>
+                <TaskInfoEntry symbol={<PriorityIcon />} className="priority">
+                    {task.priority}
+                </TaskInfoEntry>
+                <TaskInfoEntry symbol={<RepeatIcon />} className="repeat">
+                    {task.recurrenceRule}
+                </TaskInfoEntry>
+                <TaskInfoEntry symbol={<NetworkIcon />} className="depends-on">
+                    {task.dependsOn.values().toArray().join(", ")}
+                </TaskInfoEntry>
+                <TaskDateInput field="dueDate" symbol={<DueIcon />} task={task} />
+                <TaskDateInput field="doneDate" symbol={<CompletedIcon />} task={task} />
+                <TaskDateInput field="cancelledDate" symbol={<CancelledIcon />} task={task} />
+                <TaskDateInput field="scheduledDate" symbol={<ScheduledIcon />} task={task} />
+                <TaskDateInput field="startDate" symbol={<StartedIcon />} task={task} />
+                <TaskDateInput field="createdDate" symbol={<CreatedIcon />} task={task} />
             </div>
         </div>
     );
